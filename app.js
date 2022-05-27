5b2a36fe0e4912b138c4a814b87b3360cf33bbc2
Sergey <hdmaxset@gmail.com>
2022-05-27 18:06:51 +0300

    refactor: delete tag selectors and add bem class naming according to rules 3.1-3.4 in html-and-css.md and 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -8,9 +8,9 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var taskInput=document.getElementById("new-task-input");//Add a new task.
+var addButton=document.getElementById("new-task-btn");//first button
+var incompleteTaskHolder=document.getElementById("incompleted-tasks");//ul of #incompleted-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -18,32 +18,39 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+    listItem.classList.add('todo-app__item');
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+    checkBox.classList.add('todo-app__checkbox');
     //label
     var label=document.createElement("label");//label
+    label.classList.add('todo-app__label');
     //input (text)
     var editInput=document.createElement("input");//text
+    editInput.classList.add('input-text');
     //button.edit
     var editButton=document.createElement("button");//edit button
+    editButton.classList.add('button');
 
     //button.delete
     var deleteButton=document.createElement("button");//delete button
     var deleteButtonImg=document.createElement("img");//delete button image
+    deleteButton.classList.add('button');
+    deleteButtonImg.classList.add('button__img');
 
     label.innerText=taskString;
-    label.className='task';
+    label.classList.add('todo-app__label', 'todo-app__edit-task');
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.classList.add('input-text', 'todo-app__edit-task');
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.classList.add('edit');
 
-    deleteButton.className="delete";
+    deleteButton.classList.add('delete');
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -85,11 +92,11 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".edit");
-    var containsClass=listItem.classList.contains("editMode");
-    //If class of the parent is .editmode
+    var containsClass=listItem.classList.contains("edit-mode");
+    //If class of the parent is .edit-mode
     if(containsClass){
 
-        //switch to .editmode
+        //switch to .edit-mode
         //label becomes the inputs value.
         label.innerText=editInput.value;
         editBtn.innerText="Edit";
@@ -98,8 +105,8 @@ var editTask=function(){
         editBtn.innerText="Save";
     }
 
-    //toggle .editmode on the parent.
-    listItem.classList.toggle("editMode");
+    //toggle .edit-mode on the parent.
+    listItem.classList.toggle("edit-mode");
 };
 
 
